c7317ad0f25230c5573ef4920150f84858d9b8f6
Carmine Zaccagnino <carmine@carminezacc.com>
2024-11-10 15:32:39 +0100

    fix typo
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _MainAppState extends State<MainApp> {
                     "https://static.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg/revision/latest?cb=20190313021755"),
               ),
               title: const Text(
-                "Start Wars Expert",
+                "Star Wars Expert",
                 style: TextStyle(color: Colors.white),
               ),
               subtitle: loading
